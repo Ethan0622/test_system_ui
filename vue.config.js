aa9094ee157726707a5fdc927394c9f864722728
Ethan <zc15372090638@gmail.com>
2022-04-01 16:55:34 +0800

    feat：更新网页title

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,9 @@ module.exports = {
   transpileDependencies: ['quasar'],
   chainWebpack: (config) => {
     config.cache(true)
+    config.plugin('html').tap((args) => {
+      args[0].title = '现代教育技术自适应测试系统'
+      return args
+    })
   },
 }
